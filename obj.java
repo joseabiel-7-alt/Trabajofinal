88784eda816bf2a5b390e48ec540ebdcb4439171
meliodas2210 <jose_abiel-7@hotmail.com>
2026-05-12 12:46:59 -0500

    Add files via upload

---
+++ obj.java
@@ -0,0 +1,206 @@
+public class obj {
+    private String cedula;
+    private String nombre;
+    private String apellido;
+    private String telefono;
+    private String direccion;
+    private String licenciaConduccion;
+    private String placa;
+    private String marca;
+    private int modelo;
+    private float precioDiario;
+    private String estado;
+    private String tipoCombustible;
+    private String transmision;
+    private String traccion;
+    private float capacidadMaletero;
+    private String Idcontrato;
+    private String cedulaCliente;
+    private String placaVehiculo;
+    private String fechaInicio;
+    private String fechaFin;    
+    private int totalDias;
+    private float valortotal;
+
+    public obj() {
+    }
+
+    public String getCedula() {
+        return cedula;
+    }
+
+    public void setCedula(String cedula) {
+        this.cedula = cedula;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public String getLicenciaConduccion() {
+        return licenciaConduccion;
+    }
+
+    public void setLicenciaConduccion(String licenciaConduccion) {
+        this.licenciaConduccion = licenciaConduccion;
+    }
+
+    public String getPlaca() {
+        return placa;
+    }
+
+    public void setPlaca(String placa) {
+        this.placa = placa;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public int getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(int modelo) {
+        this.modelo = modelo;
+    }
+
+    public float getPrecioDiario() {
+        return precioDiario;
+    }
+
+    public void setPrecioDiario(float precioDiario) {
+        this.precioDiario = precioDiario;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    public String getTipoCombustible() {
+        return tipoCombustible;
+    }
+
+    public void setTipoCombustible(String tipoCombustible) {
+        this.tipoCombustible = tipoCombustible;
+    }
+
+    public String getTransmision() {
+        return transmision;
+    }
+
+    public void setTransmision(String transmision) {
+        this.transmision = transmision;
+    }
+
+    public String getTraccion() {
+        return traccion;
+    }
+
+    public void setTraccion(String traccion) {
+        this.traccion = traccion;
+    }
+
+    public float getCapacidadMaletero() {
+        return capacidadMaletero;
+    }
+
+    public void setCapacidadMaletero(float capacidadMaletero) {
+        this.capacidadMaletero = capacidadMaletero;
+    }
+
+    public String getIdcontrato() {
+        return Idcontrato;
+    }
+
+    public void setIdcontrato(String Idcontrato) {
+        this.Idcontrato = Idcontrato;
+    }
+
+    public String getCedulaCliente() {
+        return cedulaCliente;
+    }
+
+    public void setCedulaCliente(String cedulaCliente) {
+        this.cedulaCliente = cedulaCliente;
+    }
+
+    public String getPlacaVehiculo() {
+        return placaVehiculo;
+    }
+
+    public void setPlacaVehiculo(String placaVehiculo) {
+        this.placaVehiculo = placaVehiculo;
+    }
+
+    public String getFechaInicio() {
+        return fechaInicio;
+    }
+
+    public void setFechaInicio(String fechaInicio) {
+        this.fechaInicio = fechaInicio;
+    }
+
+    public String getFechaFin() {
+        return fechaFin;
+    }
+
+    public void setFechaFin(String fechaFin) {
+        this.fechaFin = fechaFin;
+    }
+
+    public int getTotalDias() {
+        return totalDias;
+    }
+
+    public void setTotalDias(int totalDias) {
+        this.totalDias = totalDias;
+    }
+
+    public float getValortotal() {
+        return valortotal;
+    }
+
+    public void setValortotal(float valortotal) {
+        this.valortotal = valortotal;
+    }
+    
+
+    
+}
